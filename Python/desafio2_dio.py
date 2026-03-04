7fc7aca338c3ac2f082c3b1dac5c4db910445aba
Levi <levicamel123@gmail.com>
2026-03-04 11:16:08 -0300

    Desafio 2 em python

---
+++ Python/desafio2_dio.py
@@ -0,0 +1,4 @@
+# Lê o nome do destinatário da transferência
+nome_destinatario = input().upper()
+print(nome_destinatario)
+# TODO: Converta todas as letras da variável 'nome_destinatario' para maiúsculas e imprima o resultado
